343a3066b3044b7a87a481a96c6523715e8576d8
chunlchan <c-chan5@northwestern.edu>
2023-10-16 20:38:52 -0500

    Update EndView.vue

--- src/views/EndView.vue
+++ src/views/EndView.vue
@@ -1,8 +1,10 @@
 <template>
     <v-container>
+        <div class="d-flex flex-column align-center">
         <h1>Experiment Complete</h1>
-        <p>Thank you</p>
-        <v-btn @click="submit">Click here to return to submit your responses and return to Prolific</v-btn>
+        <p>Thank you. Click below to submit your responses and return to Prolific.</p>
+        <v-btn class="mt-4" color="primary" @click="submit">Submit <v-icon>chevron_right</v-icon></v-btn>
+        </div>
     </v-container>
 </template>
 
