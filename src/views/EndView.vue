2823d1d4aae9da201333d941ec335cd276b5929b
chunlchan <c-chan5@northwestern.edu>
2023-10-16 20:36:32 -0500

    Prolific completion URL

--- src/views/EndView.vue
+++ src/views/EndView.vue
@@ -2,6 +2,7 @@
     <v-container>
         <h1>Experiment Complete</h1>
         <p>Thank you</p>
+        <v-btn @click="submit">Click here to return to submit your responses and return to Prolific</v-btn>
     </v-container>
 </template>
 
@@ -11,4 +12,9 @@ import { onMounted } from 'vue';
 onMounted(()=>{
     window.onbeforeunload = null;
 })
+
+const submit = () => {
+    const completionUrl = "https://app.prolific.com/submissions/complete?cc=C1HDAHIY";
+    window.location.replace(completionUrl);
+}
 </script>
