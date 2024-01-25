3697ed0ca4f22296b6fd7d42b07c3b5f16233153
rstanhope <rebekahmstanhope@gmail.com>
2024-01-24 19:26:22 -0600

    lists for main experiment

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -6,7 +6,11 @@
             The prominent words are in a sense emphasized for the listener, and stand out from other non-prominent words.</p>
 
             <p>In this experiment, you will be asked to listen to speech and rate how much particular words stand out
-            from others in the sentence. In each trial, you will hear one sentence and rate two words.</p>
+            from others in the sentence. In each trial, you will read a short dialogue between two speakers and see a 
+            picture representing the situation they are talking about. Then, you will hear a spoken response to the 
+            last part of the conversation you just read. You will rate two words in the spoken response according to 
+            how much they stand out from others in the sentence. Occasionally, instead of rating words, you will be asked
+            to answer a question about the dialogue you read.</p>
 
             <p>You will start with a practice trial to familiarize yourself with the process.</p>
             
