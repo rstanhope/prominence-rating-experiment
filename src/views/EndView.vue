a2a11e9056bca4d5bbfdc68957293263b467221e
rstanhope <rebekahmstanhope@gmail.com>
2024-02-12 09:48:38 -0600

    changed Prolific to Sona in EndView

--- src/views/EndView.vue
+++ src/views/EndView.vue
@@ -2,7 +2,7 @@
     <v-container>
         <div class="d-flex flex-column align-center">
         <h1>Experiment Complete</h1>
-        <p>Thank you. Click below to submit your responses and return to Prolific.</p>
+        <p>Thank you. Click below to submit your responses and return to Sona.</p>
         <v-btn class="mt-4" color="primary" @click="submit">Submit <v-icon>chevron_right</v-icon></v-btn>
         </div>
     </v-container>
